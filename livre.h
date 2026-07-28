c0fb21b00526d45819c5c25b1fae454ebd0836ef
Paguiyedou <paguiyedousakpani@gmail.com>
2026-07-28 02:27:42 +0000

    Premier depot du projet de gestion d' une bibliotheque

---
+++ livre.h
@@ -0,0 +1,60 @@
+#ifndef LIVRE_H
+#define LIVRE_H
+
+typedef struct Livre{
+    char isbn[14];
+    char titre[100];
+    char auteur[100];
+    int annee;
+    int exemplaires_totales;
+    int exemplaires_reste;
+}livre;
+
+
+// Liste chaînée des livres
+
+typedef struct Tlivre{
+    livre L;
+    struct Tlivre *suivant;
+}liste_livre;
+
+
+// Contrôle de la collection
+
+typedef struct {
+    liste_livre *premier;
+    liste_livre *dernier;
+    int nombreLivre;
+}collection;
+
+
+// ABR des livres
+
+typedef struct abrLivre{
+    livre L;
+    struct abrLivre *gauche;
+    struct abrLivre *droit;
+}abrLivre;
+
+
+// Fonctions liste
+
+liste_livre *CreerMaillon(livre l);
+
+void AjoutLivre(collection *c);
+
+
+// Fonctions ABR
+
+abrLivre *CreerNoeud(livre l);
+
+abrLivre *InsererLivre(abrLivre *racine, livre l);
+
+abrLivre *RechercheLivre(abrLivre *racine, char isbn[]);
+
+abrLivre *SupprimerLivre(abrLivre *racine, char isbn[]);
+
+void Affichage_Trie(abrLivre *racine);
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "livre.h"
+#include "emprunt.h"
+#include "lecteur.h"
+#include "fichier.h"
+
+
+
+int main(){
+    
+    return 0;
+}
